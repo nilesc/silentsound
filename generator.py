5873e0d361a57e4d38022615062e6f6dca646f57
Niles Christensen <nnc2117@columbia.edu>
2018-11-01 15:11:38 -0400

    [Adds] dummy generator file

---
+++ generator.py
@@ -0,0 +1,11 @@
+from wavegan import wavegan
+
+class Generator:
+
+    def __init__(self):
+        """ Initializes the generator. Currently a placeholder. """    
+        print("Hello, world!")
+
+if __name__ == '__main__':
+    my_generator = Generator()
+
