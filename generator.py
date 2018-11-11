03cd27e8a84c7085d48d1efa16d3e484eec2b78c
Niles Christensen <nnc2117@columbia.edu>
2018-11-11 15:22:26 -0500

    [Adds] the start of a keras port

--- generator.py
+++ generator.py
@@ -5,28 +5,36 @@ import PIL.Image
 from IPython.display import display, Audio
 import time as time
 
+def build_generator(graph):
+    with graph.as_default():
+        placeholder = tf.placeholder(dtype=float, shape=(1, 100), name='video_input')
+        next_layer = tf.layers.dense(inputs=placeholder, units=10)
+
+        rest_of_network = WaveGANGenerator(next_layer)
+
+    return rest_of_network
+
 if __name__ == '__main__':
-    # Load the model
+
     tf.reset_default_graph()
     saver = tf.train.import_meta_graph('infer.meta')
-    graph = tf.get_default_graph()
     sess = tf.InteractiveSession()
+    graph = tf.get_default_graph()
+    build_generator(graph)
     saver.restore(sess, 'model.ckpt')
 
-    # Generate and display audio
-    
     # CHANGE THESE to change number of examples generated/displayed
     ngenerate = 64
     ndisplay = 4
-    
+
     # Sample latent vectors
     _z = (np.random.rand(ngenerate, 100) * 2.) - 1.
-    
+
     # Generate
     z = graph.get_tensor_by_name('z:0')
     G_z = graph.get_tensor_by_name('G_z:0')[:, :, 0]
     G_z_spec = graph.get_tensor_by_name('G_z_spec:0')
-    
+
     start = time.time()
     _G_z, _G_z_spec = sess.run([G_z, G_z_spec], {z: _z})
     print('Finished! (Took {} seconds)'.format(time.time() - start))
