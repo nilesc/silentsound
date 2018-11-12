87b9dac46d69f321433b6f8651e6a80b09f16f9d
Niles Christensen <nnc2117@columbia.edu>
2018-11-11 23:19:30 -0500

    [Adds] some changes that don't work

--- generator.py
+++ generator.py
@@ -8,12 +8,10 @@ import librosa
 
 def build_generator(graph):
     with graph.as_default():
-        placeholder = tf.placeholder(dtype=float, shape=(1, 100), name='video_input')
-        next_layer = tf.layers.dense(inputs=placeholder, units=10)
+#        placeholder = tf.placeholder(dtype=float, shape=(None, 10), name='video_input')
+#        next_layer = tf.layers.dense(inputs=placeholder, units=100)
 
         rest_of_network = WaveGANGenerator(next_layer)
-
-    return rest_of_network
 
 if __name__ == '__main__':
 
@@ -29,19 +27,14 @@ if __name__ == '__main__':
     ndisplay = 64
 
     # Sample latent vectors
-    _z = (np.random.rand(ngenerate, 100) * 2.) - 1.
+    _z = (np.random.rand(ngenerate, 10) * 2.) - 1.
 
     # Generate
-    z = graph.get_tensor_by_name('z:0')
+    video_input = graph.get_tensor_by_name('video_input:0')
     G_z = graph.get_tensor_by_name('G_z:0')[:, :, 0]
     G_z_spec = graph.get_tensor_by_name('G_z_spec:0')
 
-    start = time.time()
-    _G_z = sess.run(G_z, {z: _z})
+    _G_z = sess.run([video_input, G_z], {video_input: _z})
 
-    for i in range(ndisplay):
-        # print('-' * 80)
-        # print('Example {}'.format(i))
-        # display(PIL.Image.fromarray(_G_z_spec[i]))
-        # display(Audio(_G_z[i], rate=16000))
-        librosa.output.write_wav('sample_audio/{}.wav'.format(i), _G_z[i], 16000) 
+    #for i in range(ndisplay):
+    #    librosa.output.write_wav('sample_audio/{}.wav'.format(i), _G_z[i], 16000) 
