92f4494e38a3d61ccc58abc98263809b9f12d0b3
Niles Christensen <nnc2117@columbia.edu>
2018-11-13 16:29:17 -0500

    [Refactors] code to be more modular

--- generator.py
+++ generator.py
@@ -1,14 +1,14 @@
-from wavegan.wavegan import WaveGANGenerator
 import tensorflow as tf
 import numpy as np
-import PIL.Image
-from IPython.display import display, Audio
-import time as time
+from IPython.display import Audio
 import librosa
 
-def build_generator(graph):
+def build_generator():
+    video_input = tf.placeholder(dtype=float, shape=(None, 10), name='video_input')
+    next_layer = tf.layers.dense(inputs=video_input, units=100)
 
-        rest_of_network = WaveGANGenerator(next_layer)
+    saver = tf.train.import_meta_graph('infer.meta', input_map={'z': next_layer})
+    saver.restore(sess, 'model.ckpt')
 
 if __name__ == '__main__':
 
@@ -16,13 +16,11 @@ if __name__ == '__main__':
 
     sess = tf.InteractiveSession()
     graph = tf.get_default_graph()
-    video_input = tf.placeholder(dtype=float, shape=(None, 10), name='video_input')
-    next_layer = tf.layers.dense(inputs=video_input, units=100)
 
-    saver = tf.train.import_meta_graph('infer.meta', input_map={'z': next_layer})
+    build_generator()
+
     init = tf.global_variables_initializer()
     sess.run(init)
-    saver.restore(sess, 'model.ckpt')
 
     # CHANGE THESE to change number of examples generated/displayed
     ngenerate = 64
@@ -31,9 +29,8 @@ if __name__ == '__main__':
     _z = (np.random.rand(ngenerate, 10) * 2.) - 1.
 
     # Generate
-    z = graph.get_tensor_by_name('z:0')
+    video_input = graph.get_tensor_by_name('video_input:0')
     G_z = graph.get_tensor_by_name('G_z:0')[:, :, 0]
-    G_z_spec = graph.get_tensor_by_name('G_z_spec:0')
 
     optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(G_z)
     _G_z = sess.run([G_z, optimizer], feed_dict={video_input: _z})
