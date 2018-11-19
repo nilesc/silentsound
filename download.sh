35bb42ab048988315cee7049f00c96753fcddcdb
Niles Christensen <nnc2117@columbia.edu>
2018-11-19 13:06:45 -0500

    [Changes] download.sh to download the datasets

--- download.sh
+++ download.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-wget https://s3.amazonaws.com/wavegan-v1/models/timit.ckpt.index -O model.ckpt.index
-wget https://s3.amazonaws.com/wavegan-v1/models/timit.ckpt.data-00000-of-00001 -O model.ckpt.data-00000-of-00001
-wget https://s3.amazonaws.com/wavegan-v1/models/timit_infer.meta -O infer.meta
+wget https://storage.cloud.google.com/avspeech-files/avspeech_train.csv
+wget https://storage.cloud.google.com/avspeech-files/avspeech_test.csv
