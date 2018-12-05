389af0e1a6a0c578afa2d350e4f7710cbfb46d7a
Niles Christensen <nnc2117@columbia.edu>
2018-12-04 21:16:28 -0500

    [Adds] a print for debugging

--- generator.py
+++ generator.py
@@ -5,13 +5,7 @@ def generate_batch(generator, weights_file, pickle_file):
     _, source_videos = beatgan.load_videos(pickle_file, beatgan.hp.window_radius, beatgan.hp.downsample_factor)
     generator.load_weights(weights_file)
     generated_audio = generator.predict(source_videos, verbose=1)
-    re_normalization_factor = 8388608
-    assumed_sample_length = 14112
-    sample_rate = 14700
-    for i in range(len(generated_audio)):
-        output = generated_audio[i]
-        q = np.array(output*re_normalization_factor).astype('int32')
-        wavfile24.write('generated_outputs/output' + ("%03d" % i) + '.wav', sample_rate, np.concatenate((q[:assumed_sample_length], q[:assumed_sample_length])), bitrate=24)
+    print(generated_audio)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
