30d0415c2de8fe4f7de2014a3f0ee9672d2393f3
Niles Christensen <nnc2117@columbia.edu>
2018-12-05 13:54:31 -0500

    [Adds] the ability to write the resulting audio

--- generator.py
+++ generator.py
@@ -1,11 +1,15 @@
 import beatgan
+from scipy.io import wavfile
 import sys
 
 def generate_batch(generator, weights_file, pickle_file):
     _, source_videos, video_ids = beatgan.load_videos(pickle_file, beatgan.hp.window_radius)
     generator.load_weights(weights_file)
-    generated_audio = generator.predict(source_videos, verbose=1)
+    generated_audio = generator.predict(source_videos, verbose=1)[0]
     print(generated_audio)
+
+    for output_audio, video_id in zip(generated_audio, video_ids):
+        wavfile.write('generated_audio/{}.wav'.format(video_id), 16384, output_audio)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
